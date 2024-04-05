459b702b1aeeb3f8231c0e33cf8b966b647b2673
amandagalemmo <agalemmo@aops.com>
2024-04-05 15:09:07 -0700

    new tilemap system

--- minesweeper/scripts/minesweeperscript.js
+++ minesweeper/scripts/minesweeperscript.js
@@ -22,8 +22,11 @@ var adjacent;  // {[tileIndex: number]: number}
 var score;
 var tiles; // 2d array of tile objects
 
+goalTileImgY = 32 * 4;
+goalTileImgX = 0;
+
 // @TODO: change to 32
-const TILEDIM = 24;                                   // Dimensions of each tile
+const TILEDIM = 32;                                   // Dimensions of each tile
 
 const sprite = new Image();
 sprite.src = 'img/guy.png';
@@ -31,9 +34,12 @@ sprite.src = 'img/guy.png';
 const flag_img = new Image();
 flag_img.src = 'img/flag.png';
 
+const tilemapImg = new Image();
+tilemapImg.src = 'img/tilemap0.png';
+
 // Palette
 var colors = {
-  hidden: '#56727a', //355070 //#964548 -- deeper red //#458396 -- blue
+  hidden: '#4188ad', //355070 //#964548 -- deeper red //#458396 -- blue
   borderDistant: '#6d597a',
   seenDistant: '#b56578',
   borderNear: '#e56b6f', 
@@ -43,6 +49,22 @@ var colors = {
 }
 
 window.onload = function() {
+  initializeBoardState();
+  console.log('loaded');
+}
+
+// UI___________________________________________________________________________
+window.addEventListener('resize', reportWindowSize);
+document.addEventListener('keydown', keyDownHandler, {once: true});
+document.addEventListener('click', clickHandler);
+
+window.addEventListener("keydown", function(e) {
+  if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+      e.preventDefault();
+  }
+}, false);
+
+function initializeBoardState() {
   canvas = document.getElementById('gameBoard');
   ctx = canvas.getContext('2d');
 
@@ -52,7 +74,11 @@ window.onload = function() {
   ROWS = Math.floor(ctx.canvas.height / TILEDIM)
   COLS = Math.floor(ctx.canvas.width / TILEDIM);
   leftPadding = Math.ceil((ctx.canvas.width - (COLS * TILEDIM)) / 2);
+  // leftPadding = Math.ceil((ctx.canvas.width - (COLS * TILEDIM)) / 2);
+  console.log("leftPadding", leftPadding);
+  // topPadding = Math.ceil((ctx.canvas.height - (ROWS * TILEDIM)) / 2);
   topPadding = Math.ceil((ctx.canvas.height - (ROWS * TILEDIM)) / 2);
+  console.log("topPadding", topPadding);
   numTiles = ROWS * COLS;
   numMines = Math.ceil(numTiles * .125); //TODO extract to variable
 
@@ -81,21 +107,9 @@ window.onload = function() {
   }
 
   setGoalTile();
-  console.log('loaded');
   move();
   draw();
 }
-
-// UI___________________________________________________________________________
-window.addEventListener('resize', reportWindowSize);
-document.addEventListener('keydown', keyDownHandler, {once: true});
-document.addEventListener('click', clickHandler);
-
-window.addEventListener("keydown", function(e) {
-  if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
-      e.preventDefault();
-  }
-}, false);
 
 function clickHandler(e) {
   let clickC = Math.floor((e.clientX - leftPadding) / TILEDIM);
@@ -107,7 +121,7 @@ function clickHandler(e) {
   } else if (!tiles[clickR][clickC].visited) {
     tiles[clickR][clickC].flagged = true;
   } else {return;}
-  drawFlag(clickR, clickC);
+  drawTile(clickR, clickC);
 }
 
 function reportWindowSize() {
@@ -159,7 +173,6 @@ function setMines() {
     while (i === playerInit || adjacent[i] === 9) {
       i = getRandomInt(numTiles);
     }
-
     // Mine is at tile i, so adjacency is 9.
     adjacent[i] = 9;
     for (let y = -COLS; y <= COLS; y += COLS) {
@@ -196,7 +209,7 @@ function setGoalTile() {
 function drawBoard() {
   for (var r = 0; r < ROWS; r++) {
     for (var c = 0; c < COLS; c++) {
-      drawFlag(r, c);
+      drawTile(r, c);
       drawPlayer();
       if (tiles[r][c].visited) {drawHint(r,c)};
     }
@@ -204,10 +217,189 @@ function drawBoard() {
 }
 
 function drawTile(r, c) {
+  // Default tile values
+  var currTile = tiles[r][c];
+  let tilemapX = 0;
+  let tilemapY = 0;
   ctx.beginPath();
-  ctx.rect(tiles[r][c].cX + leftPadding, tiles[r][c].cY + topPadding,
-    TILEDIM, TILEDIM);
-  ctx.fillStyle = tiles[r][c].color;
+  if (currTile.goalTile) {
+    if (currTile.visited) {
+      tilemapX = TILEDIM * 1;
+      tilemapY = TILEDIM * 4;
+    } else {
+      tilemapX = 0;
+      tilemapY = TILEDIM * 5;
+    }
+  } else if (currTile.flagged) {
+    tilemapX = TILEDIM * 1;
+    tilemapY = TILEDIM * 5;
+  } else {
+    if (currTile.neighbors === 0 && currTile.visited) {
+      tilemapX = TILEDIM * 2;
+      tilemapY = TILEDIM * 4;
+    } else {
+      let numNeighborsVisited = 0;
+      var nIsVisited = (tiles[r-1] && tiles[r-1][c].visited);
+      if (nIsVisited) numNeighborsVisited++;
+      var wIsVisited = (tiles[r][c-1] && tiles[r][c-1].visited);
+      if (wIsVisited) numNeighborsVisited++;
+      var sIsVisited = (tiles[r+1] && tiles[r+1][c].visited);
+      if (sIsVisited) numNeighborsVisited++;
+      var eIsVisited = (tiles[r][c+1] && tiles[r][c+1].visited);
+      if (eIsVisited) numNeighborsVisited++;
+      var nHasNeighbors = (tiles[r-1] && tiles[r-1][c].neighbors);
+      var eHasNeighbors = (tiles[r][c+1] && tiles[r][c+1].neighbors);
+      var sHasNeighbors = (tiles[r+1] && tiles[r+1][c].neighbors);
+      var wHasNeighbors = (tiles[r][c-1] && tiles[r][c-1].neighbors);
+
+      if (currTile.visited) {
+        if (!nHasNeighbors) {
+          if (!eHasNeighbors) {
+            tilemapX = TILEDIM * 2;
+            tilemapY = TILEDIM * 1;
+          } else if (!wHasNeighbors) {
+            tilemapX = TILEDIM * 0;
+            tilemapY = TILEDIM * 1;
+          } else {
+            tilemapX = TILEDIM * 1;
+            tilemapY = TILEDIM * 1;
+          }
+        } else if (!eHasNeighbors) {
+          if (!sHasNeighbors) {
+            tilemapX = TILEDIM * 2;
+            tilemapY = TILEDIM * 3;
+          } else {
+            tilemapX = TILEDIM * 2;
+            tilemapY = TILEDIM * 2;
+          }
+        } else if (!sHasNeighbors) {
+          if (!wHasNeighbors) {
+            tilemapX = TILEDIM * 0;
+            tilemapY = TILEDIM * 3;
+          } else {
+            tilemapX = TILEDIM * 1;
+            tilemapY = TILEDIM * 3;
+          }
+        } else if (!wHasNeighbors) {
+          tilemapX = TILEDIM * 0;
+          tilemapY = TILEDIM * 2;
+        }
+
+        if (numNeighborsVisited === 2) {
+          if (!nIsVisited) {
+            if (!eIsVisited) {
+              tilemapX = TILEDIM * 6;
+              tilemapY = TILEDIM * 1;
+            } else if (!wIsVisited) {
+              tilemapX = TILEDIM * 4;
+              tilemapY = TILEDIM * 1;
+            }
+          } else if (!eIsVisited) {
+            if (!sIsVisited) {
+              tilemapX = TILEDIM * 6;
+              tilemapY = TILEDIM * 3;
+            }
+          } else if (!sIsVisited) {
+            if (!wIsVisited) {
+              tilemapX = TILEDIM * 4;
+              tilemapY = TILEDIM * 3;
+            }
+          }
+        } else if (numNeighborsVisited === 3) {
+          if (!nIsVisited) {
+            tilemapX = TILEDIM * 1;
+            tilemapY = TILEDIM * 3;
+          } else if (!eIsVisited) {
+            tilemapX = TILEDIM * 0;
+            tilemapY = TILEDIM * 2;
+          } else if (!sIsVisited) {
+            tilemapX = TILEDIM * 1;
+            tilemapY = TILEDIM * 1;
+          } else if (!wIsVisited) {
+            tilemapX = TILEDIM * 2;
+            tilemapY = TILEDIM * 2;
+          }
+        } else if (numNeighborsVisited === 1) {
+          if (nIsVisited) {
+            tilemapX = TILEDIM * 9;
+            tilemapY = TILEDIM * 4;
+          } else if (eIsVisited) {
+            tilemapX = TILEDIM * 9;
+            tilemapY = TILEDIM * 1;
+          } else if (sIsVisited) {
+            tilemapX = TILEDIM * 9;
+            tilemapY = TILEDIM * 3;
+          } else if (wIsVisited) {
+            tilemapX = TILEDIM * 10;
+            tilemapY = TILEDIM * 1;
+          }
+        }
+      } else {
+        if (numNeighborsVisited === 4) {
+          tilemapX = TILEDIM * 1;
+          tilemapY = TILEDIM * 2;
+        } else if (numNeighborsVisited === 2) { // @TODO implement numVisited === 3
+          if (nIsVisited) {
+            if (eIsVisited) {
+              tilemapX = TILEDIM * 10;
+              tilemapY = TILEDIM * 9;
+            } else if (wIsVisited) {
+              tilemapX = TILEDIM * 2;
+              tilemapY = TILEDIM * 9;
+            } else if (sIsVisited) {
+              tilemapX = TILEDIM * 9;
+              tilemapY = TILEDIM * 2;
+            }
+          } else if (eIsVisited) {
+            if (sIsVisited) {
+              tilemapX = TILEDIM * 3;
+              tilemapY = TILEDIM * 9;
+            } else if (wIsVisited) {
+              tilemapX = TILEDIM * 10;
+              tilemapY = TILEDIM * 3;
+            }
+          } else if (sIsVisited) {
+            if (wIsVisited) {
+              tilemapX = TILEDIM * 7;
+              tilemapY = TILEDIM * 9;
+            }
+          }
+        } else if (numNeighborsVisited === 1) {
+          if (nIsVisited) {
+            tilemapX = TILEDIM * 5;
+            tilemapY = TILEDIM * 4;
+          } else if (eIsVisited) {
+            tilemapX = TILEDIM * 3;
+            tilemapY = TILEDIM * 2;
+          } else if (sIsVisited) {
+            tilemapX = TILEDIM * 5;
+            tilemapY = TILEDIM * 0;
+          } else if (wIsVisited) {
+            tilemapX = TILEDIM * 7;
+            tilemapY = TILEDIM * 2;
+          }
+        }
+      }
+    }
+  }
+  if (!tilemapX && !tilemapY) {
+    ctx.rect(tiles[r][c].cX + leftPadding, tiles[r][c].cY + topPadding,
+      TILEDIM, TILEDIM);
+    ctx.fillStyle = tiles[r][c].color;
+  } else {
+    ctx.drawImage(
+      tilemapImg,
+      tilemapX,
+      tilemapY,
+      TILEDIM,
+      TILEDIM,
+      currTile.cX + leftPadding,
+      currTile.cY + topPadding,
+      TILEDIM,
+      TILEDIM
+    );
+  }
+
   ctx.fill();
   ctx.closePath();
 }
@@ -231,28 +423,14 @@ function drawHint(r, c) { // i've done something stupid here
     ctx.drawImage(flag_img, tiles[r][c].cX + leftPadding, tiles[r][c].cY + topPadding);
     ctx.fill();
     ctx.closePath();
+  } else if (tiles[r][c].goalTile) {
+    return;
   } else {
     ctx.font = '12px Arial';
     ctx.fillStyle = 'white';
     ctx.fillText(tiles[r][c].neighbors>0 ? `${tiles[r][c].neighbors}` : ' ',
-    tiles[r][c].cX + 8 + leftPadding, tiles[r][c].cY + 16+ topPadding, TILEDIM);
+      tiles[r][c].cX + (TILEDIM * 0.33) + leftPadding, tiles[r][c].cY + (TILEDIM * 0.66) + topPadding, TILEDIM);
   }  
-}
-
-function drawFlag(r, c) {
-  if (tiles[r][c].flagged) {
-    ctx.beginPath();
-    ctx.drawImage(flag_img, tiles[r][c].cX + leftPadding, tiles[r][c].cY + topPadding);
-    ctx.fill();
-    ctx.closePath();
-    
-    // ctx.font = '12 px Arial';
-    // ctx.fillStyle = 'white';
-    // ctx.fillText('!',
-    //   tiles[r][c].cX+8+leftPadding, tiles[r][c].cY+16+topPadding, TILEDIM);
-  } else {
-    drawTile(r, c);
-  }
 }
 
 function draw() {
@@ -282,10 +460,10 @@ function move() {
     let currQueue = new Set();
     exposeQ.push(tiles[playerR][playerC]);
     while (exposeQ.length > 0 && exposeQ[0] !== null) {
-      let currNode = exposeQ.shift();
-      currQueue.delete(currNode);
+      let currTile = exposeQ.shift();
+      currQueue.delete(currTile);
       //handles current tile, returns list of exposure-ready neighbors
-      let result = handleTile(currNode.cY / TILEDIM, currNode.cX / TILEDIM);
+      let result = handleTile(currTile.cY / TILEDIM, currTile.cX / TILEDIM);
       if (result !== null && result.length > 0) {
         for (let r of result) {
           currQueue.add(r);
@@ -361,12 +539,13 @@ function assignColor(tile) {
 
 function handleGameOver() {
   console.log('GAME OVER!');
-  drawBoard();
+  // setTimeout(alert, 250, 'Game over! Close to try again');
+  initializeBoardState();
   drawPlayer();
-  setTimeout(alert, 250, 'Game over! Refresh to play again');
   document.removeEventListener('keydown', keyDownHandler, {once: true});
 }
 
 function handleWin() {
   console.log('YOU MADE IT!');
+  initializeBoardState();
 }
